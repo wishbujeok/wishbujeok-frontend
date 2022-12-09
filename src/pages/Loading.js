1e5b74c258c45ae1fd6067556698cbb11ae3b583
hyunjun Yang <yhj0439@nate.com>
2022-12-09 19:41:56 +0900

    Loading.js 파일 이미지 띄우기 완료

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,14 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
 import AppLayout from "../components/layout/AppLayout";
+import bujeok from "../assets/img/로딩이미지.svg";
 
 const Loading = () => {
   return (
     <div className="loading">
       <AppLayout>
-        <div>hi confirm</div>
+        <LoadingContainer>
+          <LoadingImg src={bujeok} alt="새해 부적" />
+          <MakeBujeok>부적 제작하는 중...</MakeBujeok>
+        </LoadingContainer>
       </AppLayout>
     </div>
   );
 };
 
 export default Loading;
+
+const LoadingContainer = styled.section`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  /* border: 1px solid white; */
+`;
+
+const LoadingImg = styled.img``;
+
+const MakeBujeok = styled.div`
+  margin-top: 10px;
+  color: white;
+`;
