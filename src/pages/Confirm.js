1a1cd931ca2d118a19185ee7e4ec0ecf323ec26b
pitang <wpeffect@gmail.com>
2022-12-11 17:12:12 +0900

    design: 부적생성 버튼까지 레이아웃 완료

--- src/pages/Confirm.js
+++ src/pages/Confirm.js
@@ -3,6 +3,8 @@ import AppLayout from "../components/layout/AppLayout";
 import styled from "styled-components";
 import "../components/shared/theme.css";
 import bujeok from "../assets/img/로딩이미지.svg";
+// import Button from "../components/features/Button";
+// import { IconName } from "react-icons/ai";
 
 const Confirm = () => {
   return (
@@ -12,6 +14,20 @@ const Confirm = () => {
         {/* 임시로 해놓았습니다. */}
         <LoadingImg src={bujeok} alt="새해 부적" />
         <BodyLarge>눌러서 뒷면을 확인해 보세요.</BodyLarge>
+        <Wish>
+          <TitleSmall>ㅇㅇㅇ님이 빌었던 소원이에요.</TitleSmall>
+          <Content>
+            정말 간절해요 어쩌구 저쩌구 삐용삐용
+            <br />
+            이거 참 재밌다~! 혹시 내용이 넘어가면 어떡하지?
+          </Content>
+          <BujeokBtn>
+            {/* <Link to="/loading"> */}
+            부적 저장하기
+            {/* </Link> */}
+          </BujeokBtn>
+          <Share>공유하기</Share>
+        </Wish>
       </AppLayout>
     </div>
   );
@@ -40,4 +56,64 @@ const BodyLarge = styled.div`
   line-height: 150%;
   letter-spacing: -0.07em;
   margin-top: 2vh;
+`;
+
+const Wish = styled.div``;
+
+// TitleSmall 하고 Content 왼쪽정렬해야함.
+const TitleSmall = styled.div`
+  color: white;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.07em;
+  margin-top: 6vh;
+  display: flex;
+  text-align: left;
+`;
+
+const Content = styled.div`
+  color: white;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 150%;
+  letter-spacing: -0.07em;
+  margin-top: 2vh;
+  padding: 12px 16px 16px;
+  background: rgba(255, 255, 255, 0.1);
+`;
+
+const BujeokBtn = styled.button`
+  background-color: #da234f;
+  color: white;
+  width: 327px;
+  height: 56px;
+  border-radius: 10px;
+  border: none;
+  margin-top: 8vh;
+
+  font-family: "Hahmlet";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 150%;
+  /* or 24px */
+  text-align: center;
+  letter-spacing: -0.07em;
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const Share = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+  letter-spacing: -0.07em;
+  text-align: center;
+  color: white;
+  margin-top: 6vh;
+  // border: 1px solid;
 `;
