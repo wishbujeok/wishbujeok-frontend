bc0e84d822a679d494661ef36781d0e6c9465459
pitang <wpeffect@gmail.com>
2022-12-25 12:39:09 +0900

    HOTFIX

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -30,50 +30,31 @@ const Create = () => {
   });
   // 서버에서 받아온 값을 담은 변수
 
-  const fetchData = async () => {
-    const { data } = await axios;
-    client.get(`/bujeok-management/bujeok`).catch(function (err) {
-      if (err.response) {
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-      } else if (err.request) {
-        console.log(err.request);
-      } else {
-        console.log("Error", err.message);
-      }
-      console.log(err.config);
-    });
-    console.log(data);
-    setGetData(data);
-  };
-
   useEffect(() => {
-    // axios
-    //   .get(`${process.env.REACT_APP_BACKEND_URL}/bujeok-management/bujeok`)
-    //   // 토큰값을 안보내서 get 요청이 안오는듯.
-    //   .then((res) => {
-    //     // axios.defaults.headers.common[
-    //     //   "Authorization"
-    //     // ] = `Bearer ${user.accessToken}`;
-    //     // console.log(axios.defaults.headers.common.Authorization);
-    //     console.log(res.data);
-    //     setGetData(res.data.response);
-    //   })
-    //   .catch((err) => {
-    //     if (err.response) {
-    //       console.log(err.response.data);
-    //       console.log(err.response.status);
-    //       console.log(err.response.headers);
-    //     } else if (err.request) {
-    //       console.log(err.request);
-    //     } else {
-    //       console.log("Error", err.message);
-    //     }
-    //     console.log(err.config);
-    //   });
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/bujeok-management/bujeok`)
+      // 토큰값을 안보내서 get 요청이 안오는듯.
+      .then((res) => {
+        // axios.defaults.headers.common[
+        //   "Authorization"
+        // ] = `Bearer ${user.accessToken}`;
+        // console.log(axios.defaults.headers.common.Authorization);
+        console.log(res.data);
+        setGetData(res.data.response);
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          console.log(err.request);
+        } else {
+          console.log("Error", err.message);
+        }
+        console.log(err.config);
+      });
 
-    fetchData();
     console.log(getData.memberName);
   }, []);
 
