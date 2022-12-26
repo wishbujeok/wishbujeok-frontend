8a2573fe2d65590613074b8d943f9466e2fca373
Kim Wonpyo <89769615+pitangland@users.noreply.github.com>
2022-12-27 07:24:32 +0900

    Revert "feat: redux 소셜 로그인 구현"

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/configStore";
+import store from "./components/shared/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
