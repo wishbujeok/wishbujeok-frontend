5fe70cd91b3a35084b3af1a17772bb80916c619b
pitang <wpeffect@gmail.com>
2022-12-30 13:54:27 +0900

    fix: color값 키 변경

--- src/components/features/ScreenShot.js
+++ src/components/features/ScreenShot.js
@@ -84,8 +84,8 @@ const BujeokImgContainer = styled.img`
 `;
 
 const TextWrapper = styled.div`
-  /* background-color: ${({ bgc }) => (bgc !== undefined ? bgc : "#6FA4F2")}; */
-  background-color: #6fa4f2;
+  background-color: ${({ bgc }) => (bgc !== undefined ? bgc : "#6FA4F2")};
+  // background-color: #6fa4f2;
   text-align: center;
   display: flex;
   justify-content: center;
